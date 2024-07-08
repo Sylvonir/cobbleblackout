3cbaa6c1bf38e1417efe2cce68d643ae42d1b955
Sylvonir <thomasmjzeng@gmail.com>
2024-07-08 01:40:07 -0700

    Code restructuring

--- common/src/main/kotlin/com/sylvonir/cobbleblackout/Blackout.kt
+++ common/src/main/kotlin/com/sylvonir/cobbleblackout/Blackout.kt
@@ -13,6 +13,7 @@ import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.s2c.play.GameStateChangeS2CPacket
 import net.minecraft.registry.tag.BlockTags
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
@@ -35,70 +36,11 @@ object Blackout {
         losers.filterIsInstance<PlayerBattleActor>().forEach {
             val player = it.entity ?: return@forEach
             if (player.abilities.creativeMode) return@forEach
-            //val activeStatusEffects = HashMap(player.activeStatusEffects)
-
-            //val networkHandler = player.networkHandler
-            //networkHandler.player = player.getServer()?.playerManager?.respawnPlayer(player, true) ?: return
-            //player = networkHandler.player
-
-            //MC-6431 fix, will be unnecessary in 1.21
-            //player.activeStatusEffects.putAll(activeStatusEffects)
-            //player.activeStatusEffects.forEach {
-            //    player.networkHandler.sendPacket(EntityStatusEffectS2CPacket(player.id, it.value))
-            //}
 
             player.addStatusEffect(StatusEffectInstance(StatusEffects.BLINDNESS, 40))
             player.addStatusEffect(StatusEffectInstance(StatusEffects.SLOWNESS, 40, 3))
 
-            var blockPos = player.spawnPointPosition
-            var angle = player.spawnAngle
-            val forced = player.isSpawnForced
-            var optional = Optional.empty<Vec3d>()
-            var respawnWorld = player.getServer()?.getWorld(player.spawnPointDimension)
-
-            if (blockPos != null && respawnWorld != null) {
-                optional = PlayerEntity.findRespawnPosition(respawnWorld, blockPos, angle, forced, true)
-            } else {
-                respawnWorld = player.getServer()?.overworld
-            }
-
-            if (respawnWorld != null) {
-                if (optional.isPresent) {
-                    val blockState = respawnWorld.getBlockState(blockPos)
-                    if (blockState != null && (blockState.isIn(BlockTags.BEDS) || blockState.isOf(Blocks.RESPAWN_ANCHOR))) {
-                        val vec3d = Vec3d.ofBottomCenter(blockPos).subtract(optional.get()).normalize()
-                        angle = MathHelper.wrapDegrees(MathHelper.atan2(vec3d.z, vec3d.x) * 57.2957763671875 - 90.0)
-                            .toFloat()
-                    }
-                    blockPos = optional.get().toBlockPos()
-                } else if (blockPos != null) {
-                    blockPos = respawnWorld.spawnPos
-                    player.networkHandler.sendPacket(
-                        GameStateChangeS2CPacket(
-                            GameStateChangeS2CPacket.NO_RESPAWN_BLOCK,
-                            0.0f
-                        )
-                    )
-                } else {
-                    blockPos = respawnWorld.spawnPos
-                }
-
-                var box = player.getDimensions(EntityPose.STANDING).getBoxAt(blockPos?.toCenterPos())
-
-                while (!respawnWorld.isSpaceEmpty(box) && box.minY < respawnWorld.topY) {
-                    blockPos = blockPos?.up()
-                    box = box.offset(0.0, 1.0, 0.0)
-                }
-
-                val respawnPos = blockPos?.toCenterPos()
-
-                if (respawnPos != null) {
-                    player.teleport(
-                        respawnWorld,
-                        respawnPos.x, respawnPos.y, respawnPos.z, angle, 0.0F
-                    )
-                }
-            }
+            calcRespawnPosAndTeleport(player)
 
             player.sendMessage(
                 Text.translatableWithFallback("cobbleblackout.blackoutmessage", "You blacked out!"),
@@ -108,6 +50,65 @@ object Blackout {
             //Wait for post-battle updates to finish
             afterOnServer(seconds = 0.5F) {
                 player.party().heal()
+            }
+        }
+    }
+
+    /**
+     * Mostly matches vanilla respawn behavior.
+     * Except always spawn at world spawn center
+     * instead of obeying spawnRadius gamerule,
+     * because the vanilla function for calculating it is not easily accessible,
+     * and I'm lazy.
+     */
+    private fun calcRespawnPosAndTeleport(player: ServerPlayerEntity) {
+        var blockPos = player.spawnPointPosition
+        var angle = player.spawnAngle
+        val forced = player.isSpawnForced
+        var optional = Optional.empty<Vec3d>()
+        var respawnWorld = player.getServer()?.getWorld(player.spawnPointDimension)
+
+        if (blockPos != null && respawnWorld != null) {
+            optional = PlayerEntity.findRespawnPosition(respawnWorld, blockPos, angle, forced, true)
+        } else {
+            respawnWorld = player.getServer()?.overworld
+        }
+
+        if (respawnWorld != null) {
+            if (optional.isPresent) { // If valid bed or anchor
+                val blockState = respawnWorld.getBlockState(blockPos)
+                if (blockState != null && (blockState.isIn(BlockTags.BEDS) || blockState.isOf(Blocks.RESPAWN_ANCHOR))) {
+                    val vec3d = Vec3d.ofBottomCenter(blockPos).subtract(optional.get()).normalize()
+                    angle = MathHelper.wrapDegrees(MathHelper.atan2(vec3d.z, vec3d.x) * 57.2957763671875 - 90.0)
+                        .toFloat()
+                }
+                blockPos = optional.get().toBlockPos()
+            } else if (blockPos != null) { // If invalid bed or anchor
+                blockPos = respawnWorld.spawnPos
+                player.networkHandler.sendPacket(
+                    GameStateChangeS2CPacket(
+                        GameStateChangeS2CPacket.NO_RESPAWN_BLOCK,
+                        GameStateChangeS2CPacket.DEMO_OPEN_SCREEN.toFloat()
+                    )
+                )
+            } else { // If no bed or anchor
+                blockPos = respawnWorld.spawnPos
+            }
+
+            var box = player.getDimensions(EntityPose.STANDING).getBoxAt(blockPos?.toCenterPos())
+
+            while (!respawnWorld.isSpaceEmpty(box) && box.minY < respawnWorld.topY) {
+                blockPos = blockPos?.up()
+                box = box.offset(0.0, 1.0, 0.0)
+            }
+
+            val respawnPos = blockPos?.toCenterPos()
+
+            if (respawnPos != null) {
+                player.teleport(
+                    respawnWorld,
+                    respawnPos.x, respawnPos.y, respawnPos.z, angle, 0.0F
+                )
             }
         }
     }
